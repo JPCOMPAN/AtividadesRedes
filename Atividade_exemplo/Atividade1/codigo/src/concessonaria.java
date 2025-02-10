029429fc70f906062e78b3a57b17b5c9895adf56
Sistemas <157807149+JPCOMPAN@users.noreply.github.com>
2025-02-10 19:26:31 -0300

    Initial commit

---
+++ Atividade_exemplo/Atividade1/codigo/src/concessonaria.java
@@ -0,0 +1,11 @@
+public class concessonaria {
+    public static void main(String[]args){
+        carro Meucarro = new carro("Toyota", "Corolla", 2022);
+        Meucarro.acelerar();
+        Meucarro.acelerar();
+        Meucarro.acelerar();
+        Meucarro.frear();
+        Meucarro.frear();
+        Meucarro.exibirInforamcoes();
+    }
+}
