91fdb6da1ecd9c7ec8e8c13f8a1f2e48a4851055
Sistemas <157807149+JPCOMPAN@users.noreply.github.com>
2025-02-18 08:15:09 -0300

    atualizações

--- Atividade_exemplo/Atividade1/codigo/codigo_atividade1/Com_Entrada_de_dados/src/carro.java
+++ Atividade_exemplo/Atividade1/codigo/codigo_atividade1/Com_Entrada_de_dados/src/carro.java
@@ -1,15 +1,16 @@
 public class carro {
-    String marca,modelo;
+    String marca,modelo, placa;
     int ano;
     int velocidadeAtual = 0;
 
-    public carro (String _marca, String _modelo, int _ano) {
+    public carro (String _marca, String _placa, String _modelo,  int _ano) {
         this.marca = _marca;
         this.modelo = _modelo;
         this.ano = _ano;
+        this.placa = _placa;
     }
     void exibirInformacoes() {
-        System.out.printf("Marca: %s\nModelo: %s\nAno: %d\n", marca, modelo, ano);
+        System.out.printf("Marca: %s\nModelo: %s\nAno: %d\nPlaca: %s\n", marca, modelo, ano, placa);
         System.out.printf("Velocidade atual do carro: %dKm/h", velocidadeAtual);
     }
     public void acelerar() {
--- Atividade_exemplo/Atividade1/codigo/codigo_atividade1/Com_Entrada_de_dados/src/concessionaria.java
+++ Atividade_exemplo/Atividade1/codigo/codigo_atividade1/Com_Entrada_de_dados/src/concessionaria.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class concessionaria {
     public static void main(String[]args) {
 
-        carro Meucarro = new carro("", "", 0);
+        carro Meucarro = new carro("", "", "", 0);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite a marca do seu carro:");
         Meucarro.marca = scanner.nextLine();
+        System.out.println("Digite a placa do seu carro:");
+        Meucarro.placa = scanner.nextLine();
         System.out.println("Digite o modelo do seu carro:");
         Meucarro.modelo = scanner.nextLine();
         System.out.println("Digite o ano do seu carro:");
         Meucarro.ano = scanner.nextInt();
+
+        System.out.println("Informações:");
         Meucarro.acelerar();
         Meucarro.acelerar();
         Meucarro.frear();
